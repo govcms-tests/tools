946b07bbb23e28e79ea44dd600f969d749cfbdf3
jackwrfuller <jack.fuller101@gmail.com>
2024-07-30 15:06:02 +1000

    refactor

--- module_check/audit.py
+++ module_check/audit.py
@@ -1,12 +1,32 @@
 import requests
-from bs4 import BeautifulSoup
-from extract import get_drupal_modules
-from datetime import datetime
+import re
 import concurrent.futures
+
+from bs4 import BeautifulSoup
+from datetime import datetime
 from threading import Lock
 from pprint import pprint
 
+exceptions = {
+    "consultation-consultation": "consultation",
+}
+
 mutex = Lock()
+
+
+def get_drupal_modules(composer_url: str) -> list[str]:
+    data = requests.get(composer_url).json()
+    drupal_modules = []
+    for key, value in data['require'].items():
+        # Only consider drupal modules.
+        if not re.search("^drupal/.*", key):
+            continue
+        name = key.removeprefix('drupal/')
+        if name in exceptions:
+            drupal_modules.append(exceptions[name])
+            continue
+        drupal_modules.append(name)
+    return drupal_modules
 
 
 def get_release_dates(composer_url: str):
